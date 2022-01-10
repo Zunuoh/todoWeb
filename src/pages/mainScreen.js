1879c3b8dd6860400236b7b9e6296fc61c6926fd
Zunuoh <audponu@gmail.com>
2022-01-10 22:39:15 +0000

    Mainscreen done

--- src/pages/mainScreen.js
+++ src/pages/mainScreen.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Plus, Delete, Edit } from "react-feather";
 
 const MainScreen = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="mainContainer">
+      <div className="mainHeader">
+        <div style={{ fontSize: 30 }}>SCHOOL</div>
+        <Button style={{backgroundColor:"#77BEBB"}}>Add Task</Button>
+      </div>
+
+      <div className="tasks">
+        <div>Tasks</div>
+
+        <div className="taskDiv">
+          <div style={{ flex: 9 }}>
+            <p>dkjddl</p>
+          </div>
+          <div style={{ flex: 1, display: "flex", flexDirection: "row" }}>
+            <div style={{ flex: 1 }}>
+              <Delete />
+            </div>
+            <div style={{ flex: 1 }}>
+              <Edit />
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default MainScreen;
